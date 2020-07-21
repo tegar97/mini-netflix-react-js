fb72cc8c84fc9c5ae696783a5511e0e69ba1283e
Muhammad Tegar Akmal <eategar@gmail.com>
2020-07-21 17:59:06 +0700

    hapus img

--- src/component/Card/Card.component.js
+++ src/component/Card/Card.component.js
@@ -16,10 +16,7 @@ import React, { Component } from 'react'
                 <div className={CardStyle.card} >
                     <img className={CardStyle.cardPoster}src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"/>
                     <img className={CardStyle.cardPoster}src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"/>
-                    <img className={CardStyle.cardPoster}src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"/>
-                    <img className={CardStyle.cardPoster}src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"/>
-                    <img className={CardStyle.cardPoster}src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"/>
-                    <img className={CardStyle.cardPoster}src="https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg"/>
+                  
                 </div>
 
            </div>
